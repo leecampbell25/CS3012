f9c8a28e36ccd8411bc3e87d810aee34ea03426b
Lee Campbell <campbel2@tcd.ie>
2018-10-02 14:42:23 +0100

    re-factoring code from online

--- Assignment 1/lca.hs
+++ Assignment 1/lca.hs
@@ -1,34 +1,16 @@
+module Tree where
+
+-- Define B-Tree as a type
 data Tree a = Empty | Node a (Tree a) (Tree a) deriving Show
+              -- either empty or it's a node that has an element and two sub-trees.
 
-myTree :: Tree Int
-myTree =
-  Node 1
-    (Node 2
-        (Node 4 Empty Empty)
-        (Node 5 Empty Empty))
-    (Node 3
-        (Node 6 Empty Empty)
-        (Node 7 Empty Empty))
+-- Plant tree
+plant :: [Int] -> Tree Int
+plant (x:[]) = (Node x Empty Empty)
+plant (x:xs) = branch x (plant xs)
 
-        lca :: Eq a => Tree a -> a -> a -> Either Bool a
-        lca Empty _ _ = Left False
-        lca (Node v tl tr) n1 n2 =
-            let l = lca tl n1 n2
-                r = lca tr n1 n2
-                onroot = v == n1 || v == n2
-            in case (l, r, onroot) of
-                (Right a  , _         , _    ) -> Right a
-                (_        , Right a   , _    ) -> Right a
-                (Left True, Left True , _    ) -> Right v
-                (Left True, _         , True ) -> Right v
-                (_        , Left True , True ) -> Right v
-                (Left True, _         , False) -> Left True
-                (_         , Left True, False) -> Left True
-                (_         , _        , True ) -> Left True
-                _ -> Left False
-
-        lca_show :: (Eq a, Show a, PrintfArg a) => Tree a -> a -> a -> String
-        lca_show t n1 n2 = printf "LCA(%d,%d)=%s" n1 n2 result
-            where result = case lca t n1 n2 of
-                                Right a -> show a
-                                _ -> "not found"
+--Grow tree
+branch :: Int -> Tree Int -> Tree Int
+branch x Empty = (Node x Empty Empty)
+branch x (Node y leftBranch rightBranch)
+      | x <= i Node i ()
