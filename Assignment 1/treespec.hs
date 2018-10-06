5bc5c444b60e91eafe4fbec8a6c8a9456d092b22
Lee Campbell <campbel2@tcd.ie>
2018-10-06 19:12:35 +0100

    Added tests for dfsMatch 1 pass, 1 failing

--- Assignment 1/treespec.hs
+++ Assignment 1/treespec.hs
@@ -19,8 +19,15 @@ main = hspec $ do
 
 main :: IO ()
 main = hspec $ do
+
   describe "Testing DFS function" $ do
     it "returns int[] indicating dfs path to a node " $ do
       dfs 1 (plant[1,2,10,12,9]) `shouldBe` [9,2,1]
     it "returns int[] indicating dfs path to a node " $ do
       dfs 5 (plant[5,6,2,4,8,20,12]) `shouldBe` [12,8,4,6,5]
+
+  describe "Testing dfsMatch fucntion" $ do
+    it "returns int[] of a matching path between two dfs paths" $ do
+      dfsMatch (dfs 1 (plant[1,2,10,12,9])) (dfs 1 (plant[1,2,10,12,9]))`shouldBe` [9,2,1]
+    it "find the common steps in the dfs paths of node 1 and node 2" $ do
+      dfsMatch (dfs 1 (plant[1,2,10,12,9])) (dfs 2 (plant[1,2,10,12,9]))`shouldBe` [9,2]
